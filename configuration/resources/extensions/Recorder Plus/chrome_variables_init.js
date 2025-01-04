98ee743ea6c168587f8d7e042beae6e54ffffcb1
r00l <r00l@no-email.no>
2025-01-04 17:43:30 +0100

    ad subm

---
+++ configuration/resources/extensions/Recorder Plus/chrome_variables_init.js
@@ -0,0 +1,16 @@
+katalonServerPort = '50000';
+katalonOnOffStatus = true;
+spy_captureObjectHotKey = {
+  keyCode: 192,
+  useAltKey: true,
+  useShiftKey: false,
+  useCtrlKey: false,
+  useMetaKey: false,
+};
+spy_loadDomMapHotKey = {
+  keyCode: 192,
+  useAltKey: true,
+  useShiftKey: false,
+  useCtrlKey: true,
+  useMetaKey: false,
+};
